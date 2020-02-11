941054c8cf82cb6a8ad9d81d68e5c6a2796bc2ad
Dmytrozn <Dmytro.zn.gmail.com>
2020-02-11 22:14:50 +0200

    4 commit

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ class TestSum(unittest.TestCase):
         '''
         data = [Fraction(1,4), Fraction(1, 4), Fraction(2,5)]
         result = sum(data)
-        self.assertEqual(result, Fraction(8, 10))
+        self.assertEqual(result, Fraction(9, 10))
 
 if __name__ == '__main__':
     unittest.main()
